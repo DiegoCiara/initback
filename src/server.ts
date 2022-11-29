5d9898d0881d82eb9fb20c3307fe4aab16b23705
DiegoCiara <114518504+DiegoCiara@users.noreply.github.com>
2022-11-29 10:10:06 -0300

    HTTPS

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import app from './app';
 const https = require("https");
 import {readFileSync} from "fs"
+import {join} from "path"
 https
   .createServer({
-    key: readFileSync("key.pem"),
-    cert: readFileSync("cert.pem"),
+    key: readFileSync(join(__dirname, "key.pem")),
+    cert: readFileSync(join(__dirname, "cert.pem")),
   },app)
   .listen(3333, ()=>{
     console.log('server is runing at port 4000')
